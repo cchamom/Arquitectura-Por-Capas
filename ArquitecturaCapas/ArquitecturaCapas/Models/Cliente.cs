2e48f5b9849851466062159a4478efabb2fad68d
cchamom <cchamom2@miumg.edu.gt>
2026-08-30 15:51:27 -0600

    first commit

---
+++ ArquitecturaCapas/ArquitecturaCapas/Models/Cliente.cs
@@ -0,0 +1,11 @@
+﻿namespace ArquitecturaCapas.Models
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; } = string.Empty;
+        public string NIT { get; set; } = string.Empty;
+        public string Telefono { get; set; } = string.Empty;
+
+0    }
+}
