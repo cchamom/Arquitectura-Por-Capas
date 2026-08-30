ae81bccfa75f331ae7cbf5470cd1ed5720414cbd
cchamom <cchamom2@miumg.edu.gt>
2026-08-30 17:03:51 -0600

    capturas

--- ArquitecturaCapas/ArquitecturaCapas/obj/Debug/net8.0/ArquitecturaCapas.AssemblyInfo.cs
+++ ArquitecturaCapas/ArquitecturaCapas/obj/Debug/net8.0/ArquitecturaCapas.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ArquitecturaCapas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e48f5b9849851466062159a4478efabb2fad68d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07c88c1b2062f5478b52fd2584872778da1de4a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ArquitecturaCapas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ArquitecturaCapas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
